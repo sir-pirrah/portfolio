7811cc652176e5748eecc7513153200a56352531
sir_pirrah <151651277+sir-pirrah@users.noreply.github.com>
2025-03-24 15:46:50 +0300

    Update navbar.jsx

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,31 +1,52 @@
-import React from 'react'
-import './navbar.css'
-import Toggle from '../Toggle/Toggle'
-
-function navbar() {
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
   return (
-   <div className="n-wrapper">
-    <div className="n-left">
+    <div className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
         <div className="n-name">Peter</div>
-        <Toggle/>
-    </div>
-    <div className="n-right">
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
         <div className="n-list">
-            <ul style={{listStyleType: 'none'}}>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Experience</li>
-                <li>Portfolio</li>
-                <li>Testimonials</li>
-            </ul>    
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Testimonial
+              </Link>
+            </li>
+          </ul>
         </div>
-        <button className="button n-button">
-            Contact 
-        </button>
+        <Link to="contact" spy={true} smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+      </div>
     </div>
-    
-    </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
